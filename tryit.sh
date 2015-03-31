12b4c8d2e8a88735743fdf8870cf6b4bf7bcab0d
geogito <geoffreygit@gmail.com>
2015-03-31 14:56:24 +0300

    test

---
+++ tryit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+# usage: connection to  a repo for pullin request.
+#mkdir -p "myDemo"
+#cd "tryouts"
+
+#git  pull http://github.com:geogito/tryouts.git 
+
+git push http://github.com/geoeito/tryout.git
+
+#USERNAME = 
+"geogito"
+ #PASSWORD = 
+ "//153245-gito"
